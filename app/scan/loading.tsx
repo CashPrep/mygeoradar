63486e5ae8954f26ff7df0c199b1128aaa31d8b8
Andrew G <unbanked123@gmail.com>
2026-05-22 11:27:56 -0400

    Remove scan: delete app/scan/loading.tsx

--- app/scan/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function ScanLoading() {
-  return (
-    <div className="min-h-screen bg-background flex items-center justify-center">
-      <div className="w-8 h-8 border-2 border-accent border-t-transparent rounded-full animate-spin" />
-    </div>
-  )
-}
