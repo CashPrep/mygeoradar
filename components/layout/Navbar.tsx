7a53a550fd00bfd1860424929a004a3b1f66569d
Andrew G <unbanked123@gmail.com>
2026-06-01 15:46:42 -0400

    feat: add Free Scan link to navbar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from 'react'
 import { Menu, X } from 'lucide-react'
 
 const navLinks = [
+  { href: '/',        label: 'Free Scan' },
   { href: '/blog',    label: 'Blog' },
   { href: '/about',   label: 'About' },
   { href: '/reviews', label: 'Reviews' },
